725daf2594213981b1f255bc33c9b2f1dd54497b
Sean Heckathorne <heckathorne.s@gmail.com>
2022-03-24 14:20:31 -0500

    changed 2021 to current db

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const DB_NAME = 'fbhl-2021'
+const DB_NAME = 'fbhl'
 const MONGO_DB_URI = `mongodb+srv://fullstack:${process.env.REACT_APP_MONGO_PASSWORD}@cluster0.ltofs.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 
 module.exports = {

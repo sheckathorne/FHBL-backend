2c394a92df9f03f1e1e8fdebc7ca52aff9f84464
Sean Heckathorne <heckathorne.s@gmail.com>
2022-07-17 17:22:17 -0500

    created match skeleton endpoint for querying by timestamp

--- controllers/matchSkeletons.js
+++ controllers/matchSkeletons.js
@@ -1,9 +1,19 @@
 const matchSkeletonsRouter = require('express').Router()
+const { response } = require('express')
 const Match = require('../models/match')
 
-matchSkeletonsRouter.get('/', async (_request, response) => {
+matchSkeletonsRouter.get('/', async (_req, res) => {
   const matches = await Match.find({}).select('matchId timestamp matchDate matchDateString clubs.clubId clubs.data.goals')
-  response.json(matches)
+  res.json(matches)
+})
+
+matchSkeletonsRouter.get('/timestamps', async (req, res) => {
+  const startTimestamp = parseInt(req.query.startTimestamp)
+  const endTimestamp = parseInt(req.query.endTimestamp)
+  const matches = await Match
+    .find({timestamp: { $gt: startTimestamp, $lt: endTimestamp }})
+    .select('matchId timestamp matchDate matchDateString clubs.clubId clubs.data.goals')
+  res.json(matches)
 })
 
 module.exports = matchSkeletonsRouter

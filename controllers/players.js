7ef07e5e13fb7c42d1c23c2d8aa96e65c0e44e22
Sean Heckathorne <heckathorne.s@gmail.com>
2022-07-21 21:11:48 -0500

    added endpoints for player search

--- controllers/players.js
+++ controllers/players.js
@@ -1,4 +1,5 @@
 const playersRouter = require('express').Router()
+const { json } = require('express')
 const Player = require('../models/player')
 
 playersRouter.get('/', async (_req, res) => {
@@ -23,12 +24,6 @@ playersRouter.get('/pagination', async (req, res) => {
   const sort = req.query.sort === 'desc' ? -1 : 1
   const skater = req.query.skater === 'true' ? true : false
   const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
-
-  console.log('skater', skater)
-  console.log('statName', statName)
-  console.log('gamesPlayed', gamesPlayed)
-  console.log('skip', skip)
-  console.log('playerCount', playerCount)
 
   const players = await Player
     .aggregate([
@@ -61,18 +56,85 @@ playersRouter.get('/pagination/club', async (req, res) => {
   res.json(players)
 })
 
-playersRouter.get('/count', async (req, res) => {
+playersRouter.get('/pagination/count', async (req, res) => {
   const skater = req.query.skater === 'true' ? true : false
   const count = await Player.find({'skater': skater}).count()
   res.json({ 'count': count })
 })
 
-playersRouter.get('/count/club', async (req, res) => {
+playersRouter.get('/pagination/club/count', async (req, res) => {
   const clubId = req.query.clubId
   const skater = req.query.skater === 'true' ? true : false
   const count = await Player.aggregate([
     { $addFields: { clubId: { $first: '$teams' } } },
     { $match: { 'skater': skater, 'clubId': clubId } },
+    { $count: 'players' }
+  ])
+
+  res.json({ 'count': count[0].players })
+})
+
+
+
+
+playersRouter.get('/search', async (req, res) => {
+  const pageNum = parseInt(req.query.pageNum) - 1
+  const playerCount = parseInt(req.query.playerCount)
+  const skip = pageNum * playerCount
+  const statName = req.query.statName
+  const sort = req.query.sort === 'desc' ? -1 : 1
+  const skater = req.query.skater === 'true' ? true : false
+  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
+  const name = req.query.name
+
+  const players = await Player
+    .aggregate([
+      { $match: { 'skater': skater, 'playerName': { '$regex': name, '$options': 'i' } } },
+      { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
+      { $skip: skip },
+      { $limit: playerCount }
+    ])
+
+  return res.json(players)
+})
+
+playersRouter.get('/search/count', async (req, res) => {
+  const skater = req.query.skater === 'true' ? true : false
+  const name = req.query.name
+  const count = await Player.find({ 'skater': skater, 'playerName': { '$regex': name, '$options': 'i' } }).count()
+  res.json({ 'count': count })
+})
+
+playersRouter.get('/search/club', async (req, res) => {
+  const pageNum = parseInt(req.query.pageNum) - 1
+  const playerCount = parseInt(req.query.playerCount)
+  const skip = pageNum * playerCount
+  const statName = req.query.statName
+  const sort = req.query.sort === 'desc' ? -1 : 1
+  const skater = req.query.skater === 'true' ? true : false
+  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
+  const name = req.query.name
+  const clubId = req.query.clubId
+
+  const players = await Player
+  .aggregate([
+    { $addFields: { clubId: { $first: '$teams' } } },
+    { $match: { 'skater': skater, 'clubId': clubId, 'playerName': { '$regex': name, '$options': 'i' } } },
+    { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
+    { $skip: skip },
+    { $limit: playerCount }
+  ])
+
+  return res.json(players)
+})
+
+playersRouter.get('/search/club/count', async (req, res) => {
+  const clubId = req.query.clubId
+  const skater = req.query.skater === 'true' ? true : false
+  const name = req.query.name  
+  const count = await Player.aggregate([
+    { $addFields: { clubId: { $first: '$teams' } } },
+    { $match: { 'skater': skater, 'clubId': clubId, 'playerName': { '$regex': name, '$options': 'i' } } },
     { $count: 'players' }
   ])
 

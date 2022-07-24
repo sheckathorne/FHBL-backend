5318bda737bcf83c240342b384742d13c916c695
Sean Heckathorne <heckathorne.s@gmail.com>
2022-07-24 09:43:23 -0500

    consolidated endpoints for player search

--- controllers/players.js
+++ controllers/players.js
@@ -1,10 +1,62 @@
 const playersRouter = require('express').Router()
-const { json } = require('express')
 const Player = require('../models/player')
 
-playersRouter.get('/', async (_req, res) => {
-  const players = await Player.find({})
+playersRouter.get('/pagination', async (req, res) => {
+  const pageNum = parseInt(req.query.pageNum) - 1
+  const playerCount = parseInt(req.query.playerCount)
+  const skip = pageNum * playerCount
+  const statName = req.query.statName
+  const sort = req.query.sort === 'desc' ? -1 : 1
+  const skater = req.query.skater === 'true' ? true : false
+  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
+  const name = req.query.name
+  const clubId = req.query.clubId
+
+  const players = await Player
+    .aggregate([
+      { $addFields: { clubId: { $first: '$teams' } } },
+      { $match: { 'skater': skater } },
+      { $match: ( clubId ) ? { 'clubId': clubId } : {} },
+      { $match: ( name ) ? { 'playerName': { '$regex': name, '$options': 'i' } } : {}},
+      { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
+      { $skip: skip },
+      { $limit: playerCount }
+    ])
   res.json(players)
+})
+
+playersRouter.get('/pagination/count', async (req, res) => {
+  const clubId = req.query.clubId
+  const skater = req.query.skater === 'true' ? true : false
+  const name = req.query.name  
+  const count = await Player.aggregate([
+    { $addFields: { clubId: { $first: '$teams' } } },
+    { $match: { 'skater': skater } },
+    { $match: ( clubId ) ? { 'clubId': clubId } : {} },
+    { $match: ( name ) ? { 'playerName': { '$regex': name, '$options': 'i' } } : {}},    
+    { $count: 'players' }
+  ])
+
+  res.json({ 'count': count[0].players })
+})
+
+playersRouter.get('/pagination/indexNum', async(req, res) => {
+  const playerId = req.query.playerId
+  const statName = req.query.statName
+  const skater = req.query.skater === 'true' ? true : false
+  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
+  const sort = req.query.sort === 'desc' ? -1 : 1
+
+  const players =
+    await Player
+      .aggregate([
+        { $match: { 'skater': skater } },
+        { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
+      ])
+
+  index = players.findIndex(player => player.playerId === playerId)
+
+  res.json(index)
 })
 
 playersRouter.get('/top', async (req, res) => {
@@ -16,128 +68,6 @@ playersRouter.get('/top', async (req, res) => {
   res.json(players)
 })
 
-playersRouter.get('/pagination', async (req, res) => {
-  const pageNum = parseInt(req.query.pageNum) - 1
-  const playerCount = parseInt(req.query.playerCount)
-  const skip = pageNum * playerCount
-  const statName = req.query.statName
-  const sort = req.query.sort === 'desc' ? -1 : 1
-  const skater = req.query.skater === 'true' ? true : false
-  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
-
-  const players = await Player
-    .aggregate([
-      { $match: { 'skater': skater } },
-      { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
-      { $skip: skip },
-      { $limit: playerCount }
-    ])
-  res.json(players)
-})
-
-playersRouter.get('/pagination/club', async (req, res) => {
-  const pageNum = parseInt(req.query.pageNum) - 1
-  const playerCount = parseInt(req.query.playerCount)
-  const skip = pageNum * playerCount
-  const statName = req.query.statName
-  const sort = req.query.sort === 'desc' ? -1 : 1
-  const skater = req.query.skater === 'true' ? true : false
-  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
-  const clubId = req.query.clubId
-
-  const players = await Player
-    .aggregate([
-      { $addFields: { clubId: { $first: '$teams' } } },
-      { $match: { 'skater': skater, 'clubId': clubId } },
-      { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
-      { $skip: skip },
-      { $limit: playerCount }
-    ])
-  res.json(players)
-})
-
-playersRouter.get('/pagination/count', async (req, res) => {
-  const skater = req.query.skater === 'true' ? true : false
-  const count = await Player.find({'skater': skater}).count()
-  res.json({ 'count': count })
-})
-
-playersRouter.get('/pagination/club/count', async (req, res) => {
-  const clubId = req.query.clubId
-  const skater = req.query.skater === 'true' ? true : false
-  const count = await Player.aggregate([
-    { $addFields: { clubId: { $first: '$teams' } } },
-    { $match: { 'skater': skater, 'clubId': clubId } },
-    { $count: 'players' }
-  ])
-
-  res.json({ 'count': count[0].players })
-})
-
-playersRouter.get('/search', async (req, res) => {
-  const pageNum = parseInt(req.query.pageNum) - 1
-  const playerCount = parseInt(req.query.playerCount)
-  const skip = pageNum * playerCount
-  const statName = req.query.statName
-  const sort = req.query.sort === 'desc' ? -1 : 1
-  const skater = req.query.skater === 'true' ? true : false
-  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
-  const name = req.query.name
-
-  const players = await Player
-    .aggregate([
-      { $match: { 'skater': skater, 'playerName': { '$regex': name, '$options': 'i' } } },
-      { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
-      { $skip: skip },
-      { $limit: playerCount }
-    ])
-
-  return res.json(players)
-})
-
-playersRouter.get('/search/count', async (req, res) => {
-  const skater = req.query.skater === 'true' ? true : false
-  const name = req.query.name
-  const count = await Player.find({ 'skater': skater, 'playerName': { '$regex': name, '$options': 'i' } }).count()
-  res.json({ 'count': count })
-})
-
-playersRouter.get('/search/club', async (req, res) => {
-  const pageNum = parseInt(req.query.pageNum) - 1
-  const playerCount = parseInt(req.query.playerCount)
-  const skip = pageNum * playerCount
-  const statName = req.query.statName
-  const sort = req.query.sort === 'desc' ? -1 : 1
-  const skater = req.query.skater === 'true' ? true : false
-  const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
-  const name = req.query.name
-  const clubId = req.query.clubId
-
-  const players = await Player
-  .aggregate([
-    { $addFields: { clubId: { $first: '$teams' } } },
-    { $match: { 'skater': skater, 'clubId': clubId, 'playerName': { '$regex': name, '$options': 'i' } } },
-    { $sort: { 'playerIsRanked': -1, [statName]: sort, [gamesPlayed]: -1 } },
-    { $skip: skip },
-    { $limit: playerCount }
-  ])
-
-  return res.json(players)
-})
-
-playersRouter.get('/search/club/count', async (req, res) => {
-  const clubId = req.query.clubId
-  const skater = req.query.skater === 'true' ? true : false
-  const name = req.query.name  
-  const count = await Player.aggregate([
-    { $addFields: { clubId: { $first: '$teams' } } },
-    { $match: { 'skater': skater, 'clubId': clubId, 'playerName': { '$regex': name, '$options': 'i' } } },
-    { $count: 'players' }
-  ])
-
-  res.json({ 'count': count[0].players })
-})
-
 playersRouter.get('/topByStat', async (req, res) => {
   const statName = req.query.statName
   const statFieldName = `${statName}_rank`
@@ -146,16 +76,16 @@ playersRouter.get('/topByStat', async (req, res) => {
   const skater = req.query.skater === 'true' ? true : false
   const gamesPlayed = skater ? 'skGamesPlayed' : 'gkGamesPlayed'
   
-
   const players = await Player
-  .aggregate([
-    { $match: { 'skater': skater, [statFieldName]: { $lte: topCount }, [statFieldName]: { $gt: 0 } } },
-    { $sort: { [statName]: sort, [gamesPlayed]: -1 } },
-    { $limit: topCount },
-    { $project: {'playerName': 1, 'value': `$${statName}`, 'rank': `$${statFieldName}`, 'playerId': 1 } }
-  ])
+    .aggregate([
+      { $match: { 'skater': skater, [statFieldName]: { $lte: topCount }, [statFieldName]: { $gt: 0 } } },
+      { $sort: { [statName]: sort, [gamesPlayed]: -1 } },
+      { $limit: topCount },
+      { $project: {'playerName': 1, 'value': `$${statName}`, 'rank': `$${statFieldName}`, 'playerId': 1 } }
+    ])
 
   return res.json(players)
 })
+
 
 module.exports = playersRouter
